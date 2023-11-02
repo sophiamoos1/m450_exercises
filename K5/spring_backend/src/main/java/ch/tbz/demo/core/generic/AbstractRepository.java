a81eac10f19759097b2fe6fdb032ab4c569d22f5
sophiamoos1 <sophia.moos@outlook.com>
2023-11-02 11:40:00 +0100

    error on project

--- K5/spring_backend/src/main/java/ch/tbz/demo/core/generic/AbstractRepository.java
+++
@@ -1,7 +0,0 @@
-package ch.tbz.demo.core.generic;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AbstractRepository<T extends AbstractEntity> extends JpaRepository<T, UUID> {
-}
--- K5/spring_backend/src/main/java/ch/tbz/demo/domain/addressbook/AddressbookService.java
+++
@@ -1,9 +0,0 @@
-package ch.tbz.demo.domain.addressbook;
-
-import ch.tbz.demo.core.generic.AbstractService;
-
-public interface AddressbookService extends AbstractService<Addressbook> {
-  Addressbook register(Addressbook addressbook);
-
-  Addressbook registerUser(Addressbook addressbook);
-}
